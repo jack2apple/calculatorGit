c0c5c4b40587b81760d3094e83fb88232b02477b
jack <jack2apple@outlook.com>
2016-04-27 22:06:32 +0800

    all in control

---
+++ calculatorGit/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  calculatorGit
+//
+//  Created by jack on 16/4/27.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+import Foundation
--- calculatorGit/ViewController.swift
+++ calculatorGit/ViewController.swift
@@ -37,17 +37,25 @@ class ViewController: UIViewController {
         case "÷": performOperation {$1 / $0}
         case "+": performOperation {$0 + $1}
         case "−": performOperation {$1 - $0}
+        case "√": performOperation {sqrt($0)}
         default:
             break
         }
     }
     
-    func performOperation (operation: (Double,Double) -> Double) {
+    private func performOperation (operation: (Double,Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
             enter()
         }
     }
+    private func performOperation (operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
     var operandStack = Array<Double>()
     
     @IBAction func enter() {
